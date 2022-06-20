428524da447fc81b92a5a2125078c1bb508f8065
Levi Almeida <levialmeida02@outlook.com>
2022-06-20 16:59:49 -0300

    cadastro de users finalizado

--- src/index.js
+++ src/index.js
@@ -1,25 +1,51 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+ const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+ const users = [];
 
 function checksExistsUserAccount(request, response, next) {
   // Complete aqui
+  const {username} = request.headers;
+
+  const user = users.find(user => user.username === username);
+
+  if(!user)
+    return response.status(404).json({error: "User not exists"})
 }
 
 app.post('/users', (request, response) => {
   // Complete aqui
+  const {name, username} = request.body;
+
+  const userExists = users.find(user => user.username === username);
+
+  if(userExists){
+    return response.status(400).json({error: "User alredy exists"})
+  }
+
+  const newUser = {
+    id: uuidv4(),
+    name: name,
+    username: username,
+    todos: []
+  }
+
+  users.push(newUser)
+
+  return response.status(201).json(newUser)
+
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
   // Complete aqui
+
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
